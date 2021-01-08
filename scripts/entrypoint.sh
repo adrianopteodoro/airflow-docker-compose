9549b6cac6515d20f6dafd836d55d7b0613934e6
Adriano Teodoro <>
2021-01-08 14:48:23 -0300

    Add requirements on scripts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-echo "---- AIRFLOW CLEAR OLD PID ---"
+echo "---- INSTALL REQUIREMENTS ----"
+python3 -m pip install -r $AIRFLOW_HOME/scripts/requirements.txt
+echo "---- AIRFLOW CLEAR OLD PID ----"
 rm -vf $AIRFLOW_HOME/*.pid
-echo "---- AIRFLOW DB INIT ---"
+echo "---- AIRFLOW DB INIT ----"
 airflow db init
-echo "---- AIRFLOW CREATE USER ---"
+echo "---- AIRFLOW CREATE USER ----"
 airflow users create \
     --username admin \
     --firstname Admin \
@@ -11,11 +13,11 @@ airflow users create \
     --role Admin \
     --email admin@master.org \
     --password admin
-echo "---- CREATE \"$AIRFLOW_HOME/airflow.log\" FILE ---"
+echo "---- CREATE \"$AIRFLOW_HOME/airflow.log\" FILE ----"
 touch $AIRFLOW_HOME/airflow.log && chmod +rw $AIRFLOW_HOME/airflow.log
-echo "---- START SCHEDULER DAEMON ---"
+echo "---- START SCHEDULER DAEMON ----"
 airflow scheduler --daemon --stdout $AIRFLOW_HOME/airflow.log --stderr $AIRFLOW_HOME/airflow.log &
-echo "---- START WEBSERVER DAEMON ---"
+echo "---- START WEBSERVER DAEMON ----"
 airflow webserver --port 8080 --daemon --stdout $AIRFLOW_HOME/airflow.log --stderr $AIRFLOW_HOME/airflow.log &
-echo "---- LOG STDOUT ---"
+echo "---- LOG STDOUT ----"
 tail -f $AIRFLOW_HOME/airflow.log
